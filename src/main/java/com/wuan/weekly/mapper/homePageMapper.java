0654534bf09c186b1b5fee97590fc8938a5082c5
smjwx <1224097037@qq.com>
2018-04-12 13:26:21 +0800

    修改了homePageMapper里用到的数据库

--- src/main/java/com/wuan/weekly/mapper/homePageMapper.java
+++ src/main/java/com/wuan/weekly/mapper/homePageMapper.java
@@ -11,18 +11,18 @@ import com.wuan.weekly.entity.Leave;
 public interface homePageMapper {
 
 	@Insert(
-			"inset into weekly(leave_num,user_id,group_id,status,text)"
+			"inset into report(week_num,user_id,group_id,status,text)"
 			+ "values (leaveNum,userId,groupId,status,reason)")
 	public void Main(Leave leave);
 	
 	@Select(
-			"select * from weekly where user_id = #{userId} "
+			"select * from report where user_id = #{userId} "
 			+ "and status = #{status}")
 	public Leave leave(
 			@Param("userId")int user_id, 
 			@Param("status") int status);
 
-	@Select("select status from weekly where userId = #{userId}")
+	@Select("select status from report where userId = #{userId}")
 	public int getStatus(@Param("userId") int id);
 	
 }
