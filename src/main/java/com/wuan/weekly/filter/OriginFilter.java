776cff5636e5299944ab58180702a961085b8997
Ericheel <283773833@qq.com>
2018-05-24 19:04:58 +0800

    还是解决跨域问题

--- src/main/java/com/wuan/weekly/filter/OriginFilter.java
+++ src/main/java/com/wuan/weekly/filter/OriginFilter.java
@@ -28,11 +28,10 @@ public class OriginFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res,
                          FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
-        //response.reset();
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST,GET.OPTIONS,DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "*");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with,content-type");
         chain.doFilter(req, res);
     }
 
