242838d21fea63a7a4c98b154ff23525bd01606a
Ericheel <283773833@qq.com>
2018-06-21 22:06:48 +0800

    Access-Control-Allow-Headers

--- src/main/java/com/wuan/weekly/web/filter/OriginFilter.java
+++ src/main/java/com/wuan/weekly/web/filter/OriginFilter.java
@@ -29,11 +29,11 @@ public class OriginFilter implements Filter {
                          FilterChain chain) throws IOException, ServletException {
 //    	req.setCharacterEncoding("utf-8");
 //    	res.setCharacterEncoding("utf-8");
-    	HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "*");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "*");
+        response.setHeader("Access-Control-Allow-Headers", "application/json");
         chain.doFilter(req, res);
     }
 
