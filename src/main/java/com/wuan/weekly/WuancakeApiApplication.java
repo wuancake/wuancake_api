8932ff5eab467fb5e25c3e599465f7366df0d837
Ericheel <283773833@qq.com>
2018-06-28 18:54:20 +0800

    引用外部配置文件

--- src/main/java/com/wuan/weekly/WuancakeApiApplication.java
+++ src/main/java/com/wuan/weekly/WuancakeApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
-@PropertySource("file:/usr/local/application.properties")
+@PropertySource("file:/etc/application.properties")
 @SpringBootApplication
 @MapperScan("com.wuan.weekly.mapper")
 public class WuancakeApiApplication {
