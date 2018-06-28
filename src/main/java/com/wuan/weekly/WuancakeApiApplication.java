30641aba4d468e80ac96a58f62dda22ae18d62f9
Ericheel <283773833@qq.com>
2018-06-28 18:39:03 +0800

    引用外部配置文件

--- src/main/java/com/wuan/weekly/WuancakeApiApplication.java
+++ src/main/java/com/wuan/weekly/WuancakeApiApplication.java
@@ -3,7 +3,9 @@ package com.wuan.weekly;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource("file:/usr/local/application.properties")
 @SpringBootApplication
 @MapperScan("com.wuan.weekly.mapper")
 public class WuancakeApiApplication {
