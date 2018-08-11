a65e379d11911297c780eccca9a7ec7359c94822
Ericheel <34547665+Ericheel@users.noreply.github.com>
2018-08-11 19:48:30 +0800

    Update WuancakeApiApplication.java

--- src/main/java/com/wuan/weekly/WuancakeApiApplication.java
+++ src/main/java/com/wuan/weekly/WuancakeApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
-@PropertySource("file:etc/application.properties")
+@PropertySource("file:/etc/application.properties")
 @SpringBootApplication
 @MapperScan("com.wuan.weekly.mapper")
 public class WuancakeApiApplication {
